9ff1e14457a60183cca8d687e77dc80ebd7cc947
Ivan <iburimskiy@gmail.com>
2021-03-11 23:56:51 +0300

    Game process added

--- 03_ThreeWayAndTkinter/application.py
+++ 03_ThreeWayAndTkinter/application.py
@@ -1,8 +1,22 @@
+from game_process import GameProcess
 from game_view import GameView
+
 
 class Application:
     _TITLE = 'CrazyGameDev'
     _WINDOW_SIZE = '500x500'
+    _RANG = 4
 
     def __init__(self):
-        self._game_view = GameView(self._TITLE, self._WINDOW_SIZE)
+        print('Application init')
+        self._game_process = GameProcess(
+            rang=self._RANG,
+        )
+        self._game_view = GameView(
+            title=self._TITLE,
+            window_size=self._WINDOW_SIZE,
+            game_process_buttons=self._game_process.buttons
+        )
+
+    def arrange_buttons(self):
+        return self._game_process.arrange_buttons()
